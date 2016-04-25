5eb8a51b5cadead4dbc3fbd8fb580276506731ee
Sydney Smith <sydmac@gmail.com>
2016-04-25 10:11:34 -0400

    Configured custom templates for Scaffolding

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,10 @@ Bundler.require(*Rails.groups)
 
 module BlogManager
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
