e89aa39b2552eb7aba2d9fa5eba6cf8a538e4cbc
Sydney Smith <sydmac@gmail.com>
2016-04-25 17:38:59 -0400

    Fixed error on create_users.rb

--- db/migrate/20160425204238_create_users.rb
+++ db/migrate/20160425204238_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name, null: false
       t.string :last_name, null: false
-      t.string :, null:false
+      t.string :display_name, null:false
       t.string :email, null: false
       t.string :password_digest, null: false
 
