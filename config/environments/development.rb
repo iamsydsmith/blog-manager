44d98c0392022de8eb3e4b82dd8f80577cb90bb2
Sydney Smith <sydmac@gmail.com>
2016-04-27 15:54:59 -0400

    fixing S3 in development and production folders

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,13 +38,15 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  # config/environments/production.rb
 config.paperclip_defaults = {
   :storage => :s3,
-  :s3_region => 'us-east-1',
+  :s3_region => ENV['AWS_REGION'],
   :s3_credentials => {
-    :bucket => 'notebook-photos',
-    :access_key_id => 'AKIAJGQP2P5M2GKHSHFQ',
-    :secret_access_key => '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'
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,13 +77,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.paperclip_defaults = {
+config.paperclip_defaults = {
   :storage => :s3,
-  :s3_region => 'us-east-1',
+  :s3_region => ENV['AWS_REGION'],
   :s3_credentials => {
-    :bucket => 'notebook-photos',
-    :access_key_id => 'AKIAJGQP2P5M2GKHSHFQ',
-    :secret_access_key => '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'
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
+
 end
