6fdf12c40e9e16bf8e7d7b8b12d31b883b2833bb
Sydney Smith <sydmac@gmail.com>
2016-04-27 18:57:25 -0400

    added background

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
     thumb: '100x100>',
     square: '200x200#',
     medium: '300x300>'
-  }
+  }, default_url: ""
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   before_save { email.downcase! }
